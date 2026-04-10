5b34faa9ddf6da96dab3b3659e9cc5c04b791fd6
Esphoney Ondicho <eaphoney@gmail.com>
2026-04-10 11:42:54 +0300

    protecting routes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -6,3 +6,27 @@ const isAuthenticated = (req, res, next) => {
 };
 
 module.exports = { isAuthenticated };
+
+
+//will be used for protecting routes
+
+// // routes/userRouter.js or authRouter.js
+// const { Router } = require('express');
+// const router = Router();
+// const { isAuthenticated } = require('../middleware/authMiddleware');
+
+// // PUBLIC: Anyone can see this
+// router.get('/about', (req, res) => res.send("About us..."));
+
+// // PROTECTED: Only logged-in users reach the controller
+// // If isAuthenticated fails, it sends the 401 error and the controller NEVER runs
+// router.get('/dashboard', isAuthenticated, (req, res) => {
+//     res.render('dashboard', { user: req.user });
+// });
+
+// // PROTECTED: Settings page
+// router.get('/settings', isAuthenticated, (req, res) => {
+//     res.render('settings');
+// });
+
+// module.exports = router;
